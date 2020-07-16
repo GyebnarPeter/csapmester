2a404dbacc03bccdf5cd6bf25cf085e820fc1875
unknown <gyebnarpetter@gmail.com>
2020-07-16 16:17:46 +0200

    i have added the main content

--- script.js
+++ script.js
@@ -1,32 +1,22 @@
-/* MOBILE MENU TOGGLE
------------------------------------------------------------------------- */
-
-$(".nav-icon").click(() => {
-    $("header").slideToggle();
-});
-
-
 /* SLIDER
 ------------------------------------------------------------------------ */
 
-let imagesIndex = 0;
-let i = 0;
-let images = document.getElementsByClassName('slider-img');
+ let imagesIndex = 0;
+ let images = document.getElementsByClassName("slide-page");
 
-let slider = () => {
-
-    for ( i = 0; i < images.length; ++i ) {
-        images[i].style.display = 'none';
+ let slider = () => {
+    for (let i = 0; i < images.length; ++i) {
+        images[i].style.display = "none";
     }
 
-    if ( imagesIndex === images.length ) {
+    if (imagesIndex === images.length) {
         imagesIndex = 0;
     }
 
-    images[imagesIndex].style.display = 'block';
+    images[imagesIndex].style.display = "block";
     imagesIndex++;
 
     setTimeout(slider, 5000);
-}
+ }
 
-slider();
+ slider();
