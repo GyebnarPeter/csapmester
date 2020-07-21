5fd3573a3005f7705e8e005fdad1157b243c84fc
unknown <gyebnarpetter@gmail.com>
2020-07-21 13:41:11 +0200

    v.1.1.

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-/* SLIDER
------------------------------------------------------------------------- */
+/* SLIDER */
 
  let imagesIndex = 0;
  let images = document.getElementsByClassName("slide-page");
@@ -20,3 +19,37 @@
  }
 
  slider();
+
+
+ /* SCROLL TOP */
+
+let scrollTopIcon = document.querySelector(".scroll-top-icon");
+
+window.onscroll = () => {
+
+    document.documentElement.scrollTop > 100
+        ? scrollTopIcon.style.opacity = 1
+        : scrollTopIcon.style.opacity = 0;
+}
+
+scrollTopIcon.onclick = () => {
+
+    document.documentElement.scrollIntoView({
+        behavior: "smooth",
+        block: "start"
+    });
+}
+
+
+/* NAVIGATION */
+
+$("nav a").click(function(event) {
+    event.preventDefault();
+    
+    let id = $(this).attr("href");
+    let target = $(id).offset().top;
+
+    $("html").animate({
+        scrollTop: target,
+    }, 500);
+});
